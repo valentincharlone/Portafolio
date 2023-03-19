e9d4bbe808515bdde8af75a4ecbe75b637b087b0
valentin-charlone <valen-charlone@hotmail.com>
2023-03-18 22:32:02 -0300

    load

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
+
 const Home = () => {
   return (
    
     <section id='home' className='home'>
-      <div className="home-section">
-        
-        <h1 className="description">Developer Full Stack</h1>
-        
-      </div>
+
+        <div className="home-section">
+          
+          <h1 className="description">Developer Full Stack</h1>
+          
+        </div>
+      
     </section>
    
   )
